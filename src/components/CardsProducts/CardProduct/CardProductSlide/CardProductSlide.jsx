c635cbf20dc56b7c54a9960816e53091e0401726
nickswilliam <nickswill2012@gmail.com>
2023-08-31 00:33:20 -0300

    swipper folders update

--- src/components/CardsProducts/CardProduct/CardProductSlide/CardProductSlide.jsx
+++ src/components/CardsProducts/CardProduct/CardProductSlide/CardProductSlide.jsx
@@ -1,4 +1,4 @@
-import { Navigation, Pagination } from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/bundle";
